2440f6e3d3a75234b81c2280735d226e600130d7
johnepiscopo <j.episcopo@sky.com>
2019-07-01 16:18:21 +0100

    Added main.go

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var dog dog
+
+	//Pass in reference to dog
+	err := do("Rover", &dog)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(dog.String())
+	return
+}
+
+//do takes an name and an interface{} to populate if found and
+//returns an error if nothing is found
+func do(name string, entity interface{}) error {
+	m := map[string]dog{
+		"Rover": dog{Name: "Rover"},
+	}
+
+	doggy, ok := m[name]
+	if !ok {
+		return fmt.Errorf("Dog called %s has not been found", name)
+	}
+
+	//takes the dog struct and converts it to a reflect.Value{} struct
+	val := reflect.ValueOf(doggy)
+
+	//the passed in interface is converted to a reflect.Value struct
+	//which allows the updating of the passed in dog (entity) with the value
+	//of the doggy returned from the map
+	reflect.ValueOf(entity).Elem().Set(val)
+
+	return nil
+}
+
+type dog struct {
+	Name string
+}
+
+func (d *dog) String() string {
+	return fmt.Sprintf("My name is %s", d.Name)
+}
